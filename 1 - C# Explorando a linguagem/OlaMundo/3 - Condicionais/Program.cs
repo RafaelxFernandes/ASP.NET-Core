7dc2449e1a74b54c6dccece6d2dca25e36137864
RafaelxFernandes <rafaelfernandes@dcc.ufrj.br>
2023-03-24 13:37:37 -0300

    Condicionais

---
+++ 1 - C# Explorando a linguagem/OlaMundo/3 - Condicionais/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o terceiro projeto");
+
+        int idadeJoao = 16;
+        int quantidadePessoas = 42;
+
+        if (idadeJoao < 18) 
+        {
+            // João está acompanhado
+            if (quantidadePessoas > 1)
+            {
+                Console.WriteLine("Entrada concedida");
+            }
+
+            Console.WriteLine("Apenas maiores de 18 anos ou acompanhados são permitidos");
+        }
+        else
+        {
+            Console.WriteLine("Entrada concedida");
+        }
+
+        Console.WriteLine("Tecle [ENTER] para fechar");
+        Console.ReadLine();
+
+        // Escrevendo de outra forma...
+        bool acompanhado = quantidadePessoas > 1;
+
+        if (idadeJoao >= 18 || acompanhado)
+        {
+            Console.WriteLine("Entrada concedida");
+        }
+        else
+        {
+            Console.WriteLine("Apenas maiores de 18 anos ou acompanhados são permitidos");
+        }
+
+    }
+
+}
