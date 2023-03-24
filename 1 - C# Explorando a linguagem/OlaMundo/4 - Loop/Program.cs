7b067e73bfebe712450e1ac29d296d6e98ba9336
RafaelxFernandes <rafaelfernandes@dcc.ufrj.br>
2023-03-24 14:16:57 -0300

    Adicionando loops

---
+++ 1 - C# Explorando a linguagem/OlaMundo/4 - Loop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Simulação de uma poupança");
+
+        double investimento = 1000;
+
+        int mes = 1;
+
+        while (mes <= 12)
+        {
+            // Rendimento de 0.5% (0.005) ao mês
+            investimento *= 1.005;
+    
+            Console.WriteLine("No mês " + mes + " você tem " + investimento);
+            
+            mes++;
+        }
+
+        // Escrevendo de outra forma...
+
+        Console.WriteLine("\n\nComparando resultados escritos de outra forma:");
+
+        investimento = 1000;
+        for (int i = 0 ; i < 12; i++) 
+        {
+            investimento *= 1.005;
+
+            Console.WriteLine("No mês " + (i + 1) + " você tem " + investimento);
+        }
+
+        Console.WriteLine("Tecle [ENTER] para fechar...");
+        Console.ReadLine();
+    }
+}
