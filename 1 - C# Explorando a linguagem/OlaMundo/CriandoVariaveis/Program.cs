8296d1dc401e84fc81e78fb43a1d23af41ea9b77
RafaelxFernandes <rafaelfernandes@dcc.ufrj.br>
2023-03-24 11:52:13 -0300

    Adicionando segundo projeto

---
+++ 1 - C# Explorando a linguagem/OlaMundo/CriandoVariaveis/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Criando variáveis em C#");
+
+        int idade;
+        idade = 24;
+
+        Console.WriteLine(idade);
+        Console.WriteLine("Minha idade é " + idade); 
+    }
+}
