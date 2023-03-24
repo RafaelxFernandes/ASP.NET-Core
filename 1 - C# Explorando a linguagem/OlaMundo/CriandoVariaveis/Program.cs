6230ec24196cd9dfad8cb7b4311fd56a641ec9bb
RafaelxFernandes <rafaelfernandes@dcc.ufrj.br>
2023-03-24 12:37:29 -0300

    Trabalhando com vários tipos de variáveis

--- 1 - C# Explorando a linguagem/OlaMundo/CriandoVariaveis/Program.cs
+++ 1 - C# Explorando a linguagem/OlaMundo/CriandoVariaveis/Program.cs
@@ -5,16 +5,32 @@ class Programa
     {
         Console.WriteLine("Criando variáveis em C#");
 
+        // Int
         int idade;
         idade = 24;
 
         Console.WriteLine(idade);
         Console.WriteLine("Minha idade é " + idade);
 
+        // Double
         double salario;
         salario = 30.30;
 
         Console.WriteLine(salario);
+
+        // Typecast
+        int salarioInteiro;
+        salarioInteiro = (int)salario;
+
+        Console.WriteLine(salarioInteiro);
+
+        // Long
+        long grande;
+        grande = 2000000000000000000;
+
+        // Short
+        short pequeno;
+        pequeno = 15000;
 
         Console.WriteLine("Tecle [ENTER] para fechar...");
         Console.ReadLine();
